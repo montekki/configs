ecc82e7df7cf2c845c969607f4b55ac0a5e099db
Fedor Sakharov <fedor.sakharov@gmail.com>
2026-03-23 17:15:34 +0100

    Fix fish fzf bindings fallback and restore nvim last cursor

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,6 +30,16 @@ vim.api.nvim_create_autocmd('Filetype', { pattern = 'rust', command = 'set color
 -- also, show tabs nicer
 vim.opt.listchars = 'tab:^ ,nbsp:¬,extends:»,precedes:«,trail:•'
 
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local line_count = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= line_count then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 --- leader+c leader+v for copy/paste
 vim.keymap.set('n', '<leader>v', '"+p')
 vim.keymap.set('v', '<leader>c', '"+y')
